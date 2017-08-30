d9015d1fcf9aacf2e3a123170385bdc365f4ca44
LIUHE <wangziyejimo@sina.com>
2017-08-30 11:08:53 +0800

    2

---
+++ src/cn/myTest/hehe/TestHeHe.java
@@ -0,0 +1,17 @@
+package cn.myTest.hehe;
+
+public class TestHeHe {
+	public static void main(String[] args) {
+		Integer[] arr = {1,2,4};
+		Integer count = 0;
+		int x = 0;
+		int y = 0;
+		
+		for (int i = 1; i < 6; i++) {//i代表1出现的个数
+			for (int j = 1; j < 6; j++) {//j代表2出现的个数
+				
+			}
+		}
+		
+	}
+}
